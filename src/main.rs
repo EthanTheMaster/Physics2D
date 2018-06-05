3caf3efb075848fca42dbe0a51792577d78c0ba7
Ethan Lam <elmemphis2000@gmail.com>
2018-06-04 19:40:57 -0500

    Viewport/Camera can be changed and the world now uses the Cartesian coordinate system

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,9 @@ use Physics2D::physics::shapes::Circle;
 use Physics2D::physics::shapes::Line;
 use Physics2D::physics::shapes::Group;
 use Physics2D::physics::Object;
-use Physics2D::renderer::Renderable;
 use Physics2D::physics::World;
+use Physics2D::renderer::Renderable;
+use Physics2D::renderer::Camera;
 
 use std::time::Instant;
 
@@ -21,73 +22,56 @@ fn main() {
     let mut window: PistonWindow = WindowSettings::new("Hello World", [800,800]).exit_on_esc(true).build().unwrap();
     let mut world = World::new(0.0, 1.0);
 
-    let mut wall = Line::new(Vec2D::new(200.0, 400.0), Vec2D::new(600.0, 400.0));
+    let mut wall1 = Line::new(Vec2D::new(-15.0, 15.0), Vec2D::new(15.0, 15.0));
+    let mut wall2 = Line::new(Vec2D::new(15.0, 15.0), Vec2D::new(15.0, -15.0));
+    let mut wall3 = Line::new(Vec2D::new(15.0, -15.0), Vec2D::new(-15.0, -15.0));
+    let mut wall4 = Line::new(Vec2D::new(-15.0, -15.0), Vec2D::new(-15.0, 15.0));
 
-    let wall1 = Line::new(Vec2D::new(0.0, 0.0), Vec2D::new(800.0, 0.0));
-    let wall2 = Line::new(Vec2D::new(0.0, 0.0), Vec2D::new(0.0, 800.0));
-    let wall3 = Line::new(Vec2D::new(0.0, 800.0), Vec2D::new(800.0, 800.0));
-    let wall4 = Line::new(Vec2D::new(800.0, 800.0), Vec2D::new(800.0, 0.0));
-
-    let mut ball1 = Circle::new(5.0, Vec2D::new(100.0, 100.0), 10.0);
-    let mut ball2 = Circle::new(2.0, Vec2D::new(130.0, 100.0), 10.0);
-    let mut ball3 = Circle::new(3.0, Vec2D::new(100.0, 130.0), 10.0);
-    let mut ball4 = Circle::new(1.0, Vec2D::new(130.0, 130.0), 10.0);
-
-    let mut ball5 = Circle::new(5.0, Vec2D::new(300.0, 500.0), 10.0);
-    let mut ball6 = Circle::new(3.0, Vec2D::new(340.0, 500.0), 10.0);
-    let mut ball7 = Circle::new(4.0, Vec2D::new(320.0, 450.0), 10.0);
-
-    let mut ball8 = Circle::new(4.0, Vec2D::new(500.0, 550.0), 30.0);
-    ball8.set_velocity(&Vec2D::new(3.0, -1.0));
+    let ball1 = Circle::new(3.0, Vec2D::new(5.0, 5.0), 0.4);
+    let ball2 = Circle::new(3.0, Vec2D::new(9.0, 5.0), 0.4);
+    let ball3 = Circle::new(3.0, Vec2D::new(7.0, 9.0), 0.4);
 
     let mut group1 = Group::new();
-    let mut group2 = Group::new();
-    let mut square = Group::create_polygon(vec![
-        Vec2D::new(300.0, 300.0),
-        Vec2D::new(350.0, 300.0),
-        Vec2D::new(350.0, 350.0),
-        Vec2D::new(300.0, 350.0),
-        Vec2D::new(300.0, 300.0)
-    ], 10.0);
-    let mut hexagon = Group::create_polygon(vec![
-        Vec2D::new(-14.01 + 200.0, 14.27 + 600.0),
-        Vec2D::new(-19.36 + 200.0, -5.0 + 600.0),
-        Vec2D::new(-5.35 + 200.0, -19.27 + 600.0),
-        Vec2D::new(14.01 + 200.0, -14.27 + 600.0),
-        Vec2D::new(19.36 + 200.0, 5.0 + 600.0),
-        Vec2D::new(5.35 + 200.0, 19.27 + 600.0),
-        Vec2D::new(-14.01 + 200.0, 14.27 + 600.0)
-    ], 5.0);
-
     group1.add_object(ball1);
     group1.add_object(ball2);
     group1.add_object(ball3);
-    group1.add_object(ball4);
 
-    group2.add_object(ball5);
-    group2.add_object(ball6);
-    group2.add_object(ball7);
+    for i in 0..10 {
+        let mut rng = rand::thread_rng();
 
-    group1.set_velocity(&Vec2D::new(5.0, 3.0));
-    group2.set_velocity(&Vec2D::new(-5.0, 3.0));
+        let rand_x = rng.gen_range::<f64>(-10.0, 10.0);
+        let rand_y = rng.gen_range::<f64>(-10.0, 10.0);
+        let rand_rot = rng.gen_range::<f64>(0.0, 6.2831852);
+        let rand_mass = rng.gen_range::<f64>(1.0, 7.0);
+        let r = rng.gen_range::<f64>(1.0, 1.5);
+        let n = rng.gen_range::<i32>(4, 8);
 
-    world.add_object(square);
+        let rand_vx = rng.gen_range::<f64>(-0.1, 0.1);
+        let rand_vy = rng.gen_range::<f64>(-0.1, 0.1);
+
+        let mut points_polygon: Vec<Vec2D> = Vec::new();
+        for i in 0..(n+1) {
+            let x: f64 = rand_x + r * (rand_rot + i as f64 * 6.2831852 / n as f64).cos();
+            let y: f64 = rand_y +r * (rand_rot + i as f64* 6.2831852 / n as f64).sin();
+            points_polygon.push(Vec2D::new(x, y));
+        }
+        let mut polygon = Group::create_polygon(points_polygon, rand_mass);
+        polygon.set_velocity(&Vec2D::new(rand_vx, rand_vy));
+        world.add_object(polygon);
+    }
+
     world.add_object(group1);
-    world.add_object(wall);
     world.add_object(wall1);
     world.add_object(wall2);
-    world.add_object(ball8);
     world.add_object(wall3);
     world.add_object(wall4);
-    world.add_object(group2);
-    world.add_object(hexagon);
 
     while let Some(e) = window.next() {
         let prev_time = Instant::now();
         window.draw_2d(&e, |c, g| {
             clear([1.0,1.0,1.0,1.0], g);
-
-            world.render(&c, g);
+            let camera: Camera = Camera::new(-20.0, 20.0, -20.0, 20.0, c.get_view_size()[0], c.get_view_size()[1]);
+            world.render(&c, g, &camera);
             world.update();
         });
         let frame_time = Instant::now().duration_since(prev_time);
--- src/renderer.rs
+++ src/renderer.rs
@@ -3,38 +3,80 @@ use piston_window::*;
 use physics::shapes::Circle;
 use physics::shapes::Line;
 use physics::shapes::Group;
+use physics::Vec2D;
 use physics::World;
 use physics::Object;
 use physics::Collidable;
 
 use piston_window::Line as GLine;
 
+pub struct Camera {
+    pub x_min: f64,
+    pub x_max: f64,
+    pub y_min: f64,
+    pub y_max: f64,
+    pub screen_width: f64,
+    pub screen_height: f64
+}
+
+impl Camera {
+    pub fn new(x_min: f64, x_max: f64, y_min: f64, y_max: f64, screen_width: f64, screen_height: f64) -> Camera {
+        Camera {
+            x_min,
+            x_max,
+            y_min,
+            y_max,
+            screen_width,
+            screen_height,
+        }
+    }
+
+    pub fn cartesian(&self, screen_pos: &Vec2D) -> Vec2D {
+        let x = (self.x_max - self.x_min) / self.screen_width * screen_pos.x + self.x_min;
+        let y = (self.y_min - self.y_max) / self.screen_height * screen_pos.y + self.y_max;
+
+        Vec2D::new(x, y)
+    }
+
+    pub fn screen(&self, cartesian_pos: &Vec2D) -> Vec2D {
+        let x = self.screen_width / (self.x_max - self.x_min) * (cartesian_pos.x - self.x_min);
+        let y = -1.0 * self.screen_height / (self.y_max - self.y_min) * (cartesian_pos.y - self.y_max);
+
+        Vec2D::new(x, y)
+    }
+}
+
 pub trait Renderable {
-    fn render(&self, context: &Context, graphics: &mut G2d);
+    fn render(&self, context: &Context, graphics: &mut G2d, camera: &Camera);
 }
 
 pub trait RenderableObject: Renderable + Object + Collidable {}
 
 impl Renderable for Circle {
-    fn render(&self, context: &Context, graphics: &mut G2d) {
+    fn render(&self, context: &Context, graphics: &mut G2d, camera: &Camera) {
+        let center = camera.screen(&self.center);
+        let radius_x = camera.screen_width/(camera.x_max - camera.x_min) * self.radius;
+        let radius_y = camera.screen_height/(camera.y_max - camera.y_min) * self.radius;
         graphics.ellipse(&Ellipse::new(self.color),
-                         [self.center.x - self.radius, self.center.y - self.radius, 2.0*self.radius, 2.0*self.radius],
+                         [center.x - radius_x, center.y - radius_y, 2.0*radius_x, 2.0*radius_y],
                             &context.draw_state, context.transform);
     }
 }
 
 impl Renderable for Line {
-    fn render(&self, context: &Context, graphics: &mut G2d) {
+    fn render(&self, context: &Context, graphics: &mut G2d, camera: &Camera) {
+        let start_point = camera.screen(&self.start_point);
+        let end_point = camera.screen(&self.end_point);
         graphics.line(&GLine::new(self.color, 1.0),
-                        [self.start_point.x, self.start_point.y, self.end_point.x, self.end_point.y],
+                        [start_point.x, start_point.y, end_point.x, end_point.y],
                             &context.draw_state, context.transform);
     }
 }
 
 impl Renderable for Group {
-    fn render(&self, context: &Context, graphics: &mut G2d) {
+    fn render(&self, context: &Context, graphics: &mut G2d, camera: &Camera) {
         for obj in self.objects.iter() {
-            obj.render(context, graphics);
+            obj.render(context, graphics, camera);
         }
     }
 }
@@ -44,9 +86,9 @@ impl RenderableObject for Line {}
 impl RenderableObject for Group {}
 
 impl Renderable for World {
-    fn render(&self, context: &Context, graphics: &mut G2d) {
+    fn render(&self, context: &Context, graphics: &mut G2d, camera: &Camera) {
         for obj in self.objects.iter() {
-            obj.render(context, graphics);
+            obj.render(context, graphics, camera);
         }
     }
 }
